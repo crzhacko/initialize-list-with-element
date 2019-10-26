84afbfeb02a60a6023c1c6e027adc5926043e686
crzhacko <crzhacko@gmail.com>
2019-10-26 23:59:12 +0900

    Initial commit

---
+++ src/main/java/com/crzhacko/jdk8list/Main.java
@@ -0,0 +1,26 @@
+package com.crzhacko.jdk8list;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> myFamily = new ArrayList<String>(){{
+            add("Tommy");
+            add("Jerry");
+            add("Jolly");
+        }};
+        System.out.println(myFamily);
+
+        List<String> whoIsJolly = Arrays.asList("Who", "is", "Jolly");
+        System.out.println(whoIsJolly);
+
+        List<String> myDaughter = Stream.of("She", "is", "my", "daughter").collect(Collectors.toList());
+        System.out.println(myDaughter);
+    }
+
+}
